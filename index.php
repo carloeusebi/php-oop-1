3c9b866b0074fae49230cb6a1447ae4970271d5e
carloeusebi <carloeusebi@gmail.it>
2023-07-21 13:38:40 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$title = 'movies';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-oop1</title>
+</head>
+
+<body>
+    <h1><?= $title ?></h1>
+</body>
+
+</html>
